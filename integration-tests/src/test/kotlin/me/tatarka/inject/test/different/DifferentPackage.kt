c05d0080dfd8f5ef28324ead0f264e1353af0094
Evan Tatarka <evan@tatarka.me>
2020-09-16 17:32:04 -0400

    Fix nested class references

--- integration-tests/src/test/kotlin/me/tatarka/inject/test/different/DifferentPackage.kt
+++ integration-tests/src/test/kotlin/me/tatarka/inject/test/different/DifferentPackage.kt
@@ -1,10 +1,13 @@
 package me.tatarka.inject.test.different
 
 import me.tatarka.inject.annotations.Component
+import me.tatarka.inject.annotations.Inject
 import me.tatarka.inject.annotations.Provides
 import me.tatarka.inject.test.CustomScope
 
-class DifferentPackageFoo
+class DifferentPackageFoo {
+    @Inject class Factory
+}
 
 @CustomScope @Component abstract class DifferentPackageScopedComponent {
     @Provides @CustomScope fun differentPackageFoo() = DifferentPackageFoo()
