539db428af6b2e58358fd02b540974a767387ef5
Evan Tatarka <evan.tatarka@willowtreeapps.com>
2021-04-07 14:25:22 -0400

    Add test for duplicate scopes, delete unused code

--- compiler-tests/src/test/kotlin/me/tatarka/inject/test/Utils.kt
+++ compiler-tests/src/test/kotlin/me/tatarka/inject/test/Utils.kt
@@ -3,16 +3,5 @@ package me.tatarka.inject.test
 import assertk.Assert
 import assertk.assertions.isNotNull
 import assertk.assertions.message
-import java.io.File
 
 fun Assert<Throwable>.output(): Assert<String> = message().isNotNull()
-
-fun File.recursiveDelete() {
-    val files = listFiles()
-    if (files != null) {
-        for (each in files) {
-            each.recursiveDelete()
-        }
-    }
-    delete()
-}
