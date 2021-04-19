a753fcc8c924f26644e15e5ed3786b8d5e9ff006
Evan Tatarka <evan.tatarka@willowtreeapps.com>
2021-04-19 11:36:58 -0400

    Add docs on testing

--- integration-tests/common/src/test/kotlin/me/tatarka/inject/test/InheritanceTest.kt
+++ integration-tests/common/src/test/kotlin/me/tatarka/inject/test/InheritanceTest.kt
@@ -3,6 +3,7 @@ package me.tatarka.inject.test
 import assertk.assertThat
 import assertk.assertions.hasClass
 import assertk.assertions.isNotNull
+import assertk.assertions.isSameAs
 import me.tatarka.inject.annotations.Component
 import me.tatarka.inject.annotations.Provides
 import kotlin.test.Test
@@ -11,33 +12,57 @@ interface ComponentInterface {
     val foo: Foo
 }
 
-@Component abstract class InterfaceComponent : ComponentInterface
+@Component
+abstract class InterfaceComponent : ComponentInterface
 
 interface GenericComponentInterface<T> {
     val genericFoo: T
 }
 
-@Component abstract class GenericInterfaceComponent : GenericComponentInterface<Foo>
+@Component
+abstract class GenericInterfaceComponent : GenericComponentInterface<Foo>
 
 interface ProvidesComponentInterface {
     val iFoo: IFoo
         @Provides get() = Foo()
 }
 
-@Component abstract class ProvidesInterfaceComponent : ProvidesComponentInterface {
+@Component
+abstract class ProvidesInterfaceComponent : ProvidesComponentInterface {
+    abstract val foo: IFoo
+}
+
+interface ProvidesScopedComponentInterface {
+    val iFoo: IFoo
+        @CustomScope
+        @Provides get() = Foo()
+}
+
+@Component
+@CustomScope
+abstract class ProvidesScopedInterfaceComponent : ProvidesScopedComponentInterface {
     abstract val foo: IFoo
 }
 
 class InheritanceTest {
-    @Test fun generates_a_component_that_provides_a_dep_defined_in_an_implemented_interface() {
+    @Test
+    fun generates_a_component_that_provides_a_dep_defined_in_an_implemented_interface() {
         val component = InterfaceComponent::class.create()
 
         assertThat(component.foo).isNotNull()
     }
 
-    @Test fun generates_a_component_that_provides_a_dep_defined_in_a_generic_implemented_interface() {
+    @Test
+    fun generates_a_component_that_provides_a_dep_defined_in_a_generic_implemented_interface() {
         val component = GenericInterfaceComponent::class.create()
 
         assertThat(component.genericFoo).hasClass(Foo::class)
+    }
+
+    @Test
+    fun generates_a_component_that_provides_a_scoped_dep_defined_in_an_implemented_interface() {
+        val component = ProvidesScopedInterfaceComponent::class.create()
+
+        assertThat(component.foo).isSameAs(component.foo)
     }
 }
