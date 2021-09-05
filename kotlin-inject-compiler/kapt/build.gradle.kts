c40f3c6740e3fbde0490f23bdab702021429af7b
Evan Tatarka <evan@tatarka.me>
2021-09-05 16:21:29 -0400

    Set correct jvm target

--- kotlin-inject-compiler/kapt/build.gradle.kts
+++ kotlin-inject-compiler/kapt/build.gradle.kts
@@ -4,6 +4,10 @@ plugins {
     id("kotlin-inject.publish")
 }
 
+java {
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 dependencies {
     implementation(project(":kotlin-inject-runtime"))
     implementation(project(":kotlin-inject-compiler:core"))
