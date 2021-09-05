7f7e5f3213befcae0bfda70a643e350951bd6c2f
Evan Tatarka <evan@tatarka.me>
2021-09-05 16:09:32 -0400

    Target apple tests on macos agent

--- buildSrc/src/main/kotlin/kotlin-inject.merge-tests.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-inject.merge-tests.gradle.kts
@@ -1,14 +1,16 @@
-import org.gradle.api.tasks.Copy
-import org.gradle.api.tasks.testing.TestReport
 import org.gradle.kotlin.dsl.getValue
 import org.gradle.kotlin.dsl.getting
-import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.tasks.KotlinTest
 
 val tests = tasks.withType<KotlinTest>()
+val testsApple = tests.filter { it.targetName?.contains(Regex("macos|ios|tvos|watchos")) ?: false }
 
 val testReport by rootProject.tasks.getting(TestReport::class) {
     reportOn(tests)
+}
+
+val testReportApple by rootProject.tasks.getting(TestReport::class) {
+    reportOn(testsApple)
 }
 
 val copyTestResults by rootProject.tasks.getting(Copy::class) {
@@ -22,6 +24,21 @@ val copyTestResults by rootProject.tasks.getting(Copy::class) {
     }
 }
 
-tests.all {
+val copyTestResultsApple by rootProject.tasks.getting(Copy::class) {
+    testsApple.forEach { test ->
+        // Have to explicitly wire up task dependency due to https://github.com/gradle/gradle/issues/17120
+        dependsOn(test)
+        from(test.reports.junitXml.outputLocation.get().asFile) {
+            include("**/*.xml")
+            rename("(.*).xml", "$1[${test.targetName}].xml")
+        }
+    }
+}
+
+val check by tasks.getting {
     finalizedBy(testReport, copyTestResults)
+}
+
+val checkApple by rootProject.tasks.getting {
+    finalizedBy(testReportApple, copyTestResultsApple)
 }
