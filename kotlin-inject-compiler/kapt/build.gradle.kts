c5721d01a702965b13da5f0c01ff40998dedb4e6
Evan Tatarka <evan@tatarka.me>
2021-09-06 13:16:19 -0400

    Fix ci publishing

--- kotlin-inject-compiler/kapt/build.gradle.kts
+++ kotlin-inject-compiler/kapt/build.gradle.kts
@@ -4,14 +4,15 @@ plugins {
     id("kotlin-inject.publish")
 }
 
-java {
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
-
 dependencies {
     implementation(project(":kotlin-inject-runtime"))
     implementation(project(":kotlin-inject-compiler:core"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation(libs.kotlinx.metadata.jvm)
     compileOnly(libs.jdk.compiler)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
--- kotlin-inject-compiler/ksp/build.gradle.kts
+++ kotlin-inject-compiler/ksp/build.gradle.kts
@@ -4,14 +4,15 @@ plugins {
     id("kotlin-inject.publish")
 }
 
-java {
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
-
 dependencies {
     implementation(project(":kotlin-inject-runtime"))
     implementation(project(":kotlin-inject-compiler:core"))
     // for access to CompilationErrorException until ksp properly fails on errors
     implementation(libs.kotlin.compiler.embeddable)
     implementation(libs.ksp)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
