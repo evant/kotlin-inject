e40676ab50bfe9b198f763b18a4c5b9c8d939ce1
Evan Tatarka <evan@tatarka.me>
2021-10-25 18:47:58 -0400

    Another accessor bug fix

--- integration-tests/module/build.gradle.kts
+++ integration-tests/module/build.gradle.kts
@@ -1,5 +1,17 @@
+import com.google.devtools.ksp.gradle.KspTaskJS
+import com.google.devtools.ksp.gradle.KspTaskJvm
+import com.google.devtools.ksp.gradle.KspTaskMetadata
+import com.google.devtools.ksp.gradle.KspTaskNative
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("kotlin-inject.multiplatform")
+    id("kotlin-inject.detekt")
+    id("com.google.devtools.ksp")
+}
+
+dependencies {
+    ksp(project(":kotlin-inject-compiler:ksp"))
 }
 
 kotlin {
@@ -10,4 +22,22 @@ kotlin {
             }
         }
     }
+}
+
+// Generate common code with ksp instead of per-platform, hopefully this won't be needed in the future.
+// https://github.com/google/ksp/issues/567
+kotlin.sourceSets.commonMain {
+    kotlin.srcDir("build/generated/ksp/commonMain/kotlin")
+}
+tasks.withType<KspTaskJS>().configureEach {
+    enabled = false
+}
+tasks.withType<KspTaskJvm>().configureEach {
+    enabled = false
+}
+tasks.withType<KspTaskNative>().configureEach {
+    enabled = false
+}
+tasks.withType<KotlinCompile>().configureEach {
+    dependsOn(tasks.withType<KspTaskMetadata>())
 }
--- integration-tests/module/src/commonMain/kotlin/me/tatarka/inject/test/module/Models.kt
+++ integration-tests/module/src/commonMain/kotlin/me/tatarka/inject/test/module/Models.kt
@@ -1,6 +1,8 @@
 package me.tatarka.inject.test.module
 
+import me.tatarka.inject.annotations.Component
 import me.tatarka.inject.annotations.Inject
+import me.tatarka.inject.annotations.Provides
 import me.tatarka.inject.annotations.Scope
 
 @Inject
@@ -11,10 +13,22 @@ annotation class ExternalScope
 
 @ExternalScope
 @Inject
-class ScopedExternalFoo
+class ScopedExternalFoo : IExternalFoo
 
 typealias externalFunction = () -> String
 
 @Inject
 @Suppress("FunctionOnlyReturningConstant")
 fun externalFunction(foo: ExternalFoo): String = "external"
+
+interface IExternalFoo
+
+@Component
+@ExternalScope
+abstract class ExternalParentComponent {
+    val ScopedExternalFoo.bind: IExternalFoo
+        @Provides get() = this
+}
+
+@Component
+abstract class ExternalChildComponent(@Component val parent: ExternalParentComponent = ExternalParentComponent::class.create())
