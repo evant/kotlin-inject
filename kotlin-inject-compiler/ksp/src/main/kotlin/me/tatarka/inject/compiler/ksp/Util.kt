cab12f3421921731bff359feda764a9b0830ded0
Evan Tatarka <evan@tatarka.me>
2021-10-29 16:14:34 -0400

    Update to ksp 1.0.1-RC and remove multiplatform workaround

--- kotlin-inject-compiler/ksp/src/main/kotlin/me/tatarka/inject/compiler/ksp/Util.kt
+++ kotlin-inject-compiler/ksp/src/main/kotlin/me/tatarka/inject/compiler/ksp/Util.kt
@@ -1,6 +1,5 @@
 package me.tatarka.inject.compiler.ksp
 
-import com.google.devtools.ksp.processing.KSPLogger
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSAnnotation
@@ -24,21 +23,14 @@ fun KSAnnotated.annotationAnnotatedWith(packageName: String, simpleName: String)
     return null
 }
 
-fun KSAnnotated.hasAnnotation(packageName: String, simpleName: String, logger: KSPLogger? = null): Boolean {
-    return annotations.any { it.hasName(packageName, simpleName, logger) }
+fun KSAnnotated.hasAnnotation(packageName: String, simpleName: String): Boolean {
+    return annotations.any { it.hasName(packageName, simpleName) }
 }
 
-private fun KSAnnotation.hasName(packageName: String, simpleName: String, logger: KSPLogger? = null): Boolean {
+private fun KSAnnotation.hasName(packageName: String, simpleName: String): Boolean {
     // we can skip resolving if the short name doesn't match
     if (shortName.asString() != simpleName) return false
-    val type = annotationType.resolve()
-    if (type.isError) {
-        // handling for KMP common code generation: https://github.com/google/ksp/issues/632
-        logger?.warn("Failed to resolve annotation @$simpleName so it's package cannot be checked." +
-                " This may cause issues if you are using annotation with the same name in a different package.")
-        return true
-    }
-    val declaration = type.declaration
+    val declaration = annotationType.resolve().declaration
     return declaration.packageName.asString() == packageName
 }
 
@@ -125,15 +117,11 @@ fun KSType.isConcrete(): Boolean {
  * A 'fast' version of [Resolver.getSymbolsWithAnnotation]. We only care about class annotations so we can skip a lot
  * of the tree.
  */
-fun Resolver.getSymbolsWithClassAnnotation(
-    packageName: String,
-    simpleName: String,
-    logger: KSPLogger? = null,
-): Sequence<KSClassDeclaration> {
+fun Resolver.getSymbolsWithClassAnnotation(packageName: String, simpleName: String): Sequence<KSClassDeclaration> {
     suspend fun SequenceScope<KSClassDeclaration>.visit(declarations: Sequence<KSDeclaration>) {
         for (declaration in declarations) {
             if (declaration is KSClassDeclaration) {
-                if (declaration.hasAnnotation(packageName, simpleName, logger)) {
+                if (declaration.hasAnnotation(packageName, simpleName)) {
                     yield(declaration)
                 }
                 visit(declaration.declarations)
