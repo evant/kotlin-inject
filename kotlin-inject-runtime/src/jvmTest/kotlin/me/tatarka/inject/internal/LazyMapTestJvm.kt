ef6e0d17b1e48d8bb27d5a39bacfd4e581604515
Evan Tatarka <evan@tatarka.me>
2021-11-10 18:56:44 -0500

    Proper multithreading support on kotlin native
    Note: this relies on the new kotlin memory model.
    
    Fixes #167

---
+++ kotlin-inject-runtime/src/jvmTest/kotlin/me/tatarka/inject/internal/LazyMapTestJvm.kt
@@ -0,0 +1,27 @@
+package me.tatarka.inject.internal
+
+import assertk.assertThat
+import assertk.assertions.each
+import assertk.assertions.isSameAs
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+import kotlin.test.Test
+
+class LazyMapTestJvm {
+    @Test
+    fun caches_value_from_multiple_threads() {
+        val lazyMap = LazyMap()
+        val count = 100
+        val executors = Executors.newFixedThreadPool(count)
+        val results = executors.invokeAll(List(count) {
+            Callable {
+                lazyMap.get("test") { Any() }
+            }
+        }).map { it.get() }
+        executors.shutdown()
+
+        assertThat(results).each {
+            it.isSameAs(results.first())
+        }
+    }
+}
