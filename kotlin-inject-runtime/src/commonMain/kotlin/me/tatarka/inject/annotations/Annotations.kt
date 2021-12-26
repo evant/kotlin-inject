2c83f9f0acdc263a4e5fba5339aa7e9c33900e74
Evan Tatarka <evan.tatarka@willowtreeapps.com>
2021-12-26 17:53:10 -0500

    Remove explicit retention annotations to get rid of kotlin js warnings
    https://youtrack.jetbrains.com/issue/KT-41082.
    
    Fixes #181

--- kotlin-inject-runtime/src/commonMain/kotlin/me/tatarka/inject/annotations/Annotations.kt
+++ kotlin-inject-runtime/src/commonMain/kotlin/me/tatarka/inject/annotations/Annotations.kt
@@ -1,6 +1,5 @@
 package me.tatarka.inject.annotations
 
-import kotlin.annotation.AnnotationRetention.RUNTIME
 import kotlin.annotation.AnnotationTarget.ANNOTATION_CLASS
 import kotlin.annotation.AnnotationTarget.CLASS
 import kotlin.annotation.AnnotationTarget.CONSTRUCTOR
@@ -8,26 +7,20 @@ import kotlin.annotation.AnnotationTarget.FUNCTION
 import kotlin.annotation.AnnotationTarget.PROPERTY_GETTER
 import kotlin.annotation.AnnotationTarget.VALUE_PARAMETER
 
-@Retention(RUNTIME)
 @Target(CLASS, FUNCTION, CONSTRUCTOR)
 annotation class Inject
 
-@Retention(RUNTIME)
 @Target(CLASS, VALUE_PARAMETER)
 annotation class Component
 
-@Retention(RUNTIME)
 @Target(FUNCTION, PROPERTY_GETTER)
 annotation class Provides
 
-@Retention(RUNTIME)
 @Target(ANNOTATION_CLASS)
 annotation class Scope
 
-@Retention(RUNTIME)
 @Target(FUNCTION, PROPERTY_GETTER)
 annotation class IntoSet
 
-@Retention(RUNTIME)
 @Target(FUNCTION, PROPERTY_GETTER)
 annotation class IntoMap
