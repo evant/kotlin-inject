17d595b4072718990e5a3818c07a339d2467f8b6
Ralf Wondratschek <rwo@amazon.com>
2024-04-04 16:14:34 -0700

    Linux Arm64
    Add support for Linux Arm64, which is a tier 2 native target: https://kotlinlang.org/docs/native-target-support.html#tier-2

--- build-logic/src/main/kotlin/NativeTargets.kt
+++
@@ -1,7 +0,0 @@
-val nativeTargets = arrayOf(
-    "linuxX64",
-    "macosX64", "macosArm64",
-    "iosArm64", "iosX64", "iosSimulatorArm64",
-    "tvosArm64", "tvosX64", "tvosSimulatorArm64",
-    "watchosArm32", "watchosArm64", "watchosX64", "watchosSimulatorArm64",
-)
